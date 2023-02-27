49a2f535c09514220e94a3a6e671c1034146cbcb
Bryan <suprnewb@gmail.com>
2023-02-27 13:07:53 +1100

    Start widget component

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,6 +1,17 @@
+import React from "react";
 import "./Widgets.css";
-import { TwitterTimelineEmbed }
+import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
 
 export default function Widgets() {
-
+  return (
+    <div className="widgets-container">
+      <h3>What is Happening?</h3>
+      <TwitterTweetEmbed tweetId={"1235"} />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="bk"
+        options={{ height: 400 }}
+      />
+    </div>
+  );
 }
